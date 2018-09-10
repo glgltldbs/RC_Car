8ad8cfcd11a96db90568b63f1798efb647a96878
glgltldbs <glgltldbs@naver.com>
2018-09-10 13:40:39 +0900

    FPGA_ALL_TEST_2

---
+++ fpga_proj/all_test_2/hardware/all_test_2.sdk/aaa/Debug/src/helloworld.d
@@ -0,0 +1,39 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xbasic_types.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../aaa_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xbasic_types.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../aaa_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
